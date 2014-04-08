6f0009ea4eb4a5d882328ba1a633bff4052e80a6
stowe063@d.umn.edu <stowe063@d.umn.edu@12b34c65-585a-62e0-4ec5-0b54b585a8a1>
2014-04-08 20:48:34 +0000

    Created ModuleList.java

---
+++ MarsHabitatConfigurationSystem/src/mhcs/client/module/ModuleList.java
@@ -0,0 +1,34 @@
+package mhcs.client.module;
+
+/**
+ * A container for all Modules in the MHCS.
+ * @author Ryan Stowell
+ */
+public class ModuleList {
+
+	public ModuleList() {
+		// todo
+	}
+	
+	public Module getModule(int code) {
+		// todo
+		
+		return null;
+	}
+	
+	public void addModule(Module module) {
+		// todo
+	}
+	
+	public void deleteModule(int code) {
+		// todo
+	}
+	
+	public void editModule(int code, Module module) {
+		// todo
+	}
+	
+	
+	private Module[] modules;
+	public static int SIZE = 190;
+}
