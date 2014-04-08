d0e5257730f7eaa266fe955a1c97528897884804
stowe063@d.umn.edu <stowe063@d.umn.edu@12b34c65-585a-62e0-4ec5-0b54b585a8a1>
2014-04-08 21:19:25 +0000

    Editted ModuleList class

--- MarsHabitatConfigurationSystem/src/mhcs/client/module/ModuleList.java
+++ MarsHabitatConfigurationSystem/src/mhcs/client/module/ModuleList.java
@@ -6,29 +6,60 @@ package mhcs.client.module;
  */
 public class ModuleList {
 
+	/**
+	 * Default constructor. Initializes each Module in list to null.
+	 */
 	public ModuleList() {
-		// todo
-	}
-	
-	public Module getModule(int code) {
-		// todo
+		modules = new Module[SIZE];
 		
-		return null;
+		for (int i = 0; i < SIZE; i++) {
+			modules[i] = null;
+		}
 	}
 	
-	public void addModule(Module module) {
-		// todo
+	/**
+	 * Getter for a Module in the ModuleList.
+	 * @param code The Module to get.  1-190
+	 * @return The Module at index code, null if the Module is not found
+	 * @throws IndexOutOfBoundsException if code is not between 1-190
+	 */
+	public Module getModule(int code) throws IndexOutOfBoundsException {
+		if (code < 1 || code > 190) {
+			throw new IndexOutOfBoundsException("Invalid code parameter for ModuleList::getModule()");
+		}
+		
+		return modules[code];
 	}
 	
-	public void deleteModule(int code) {
-		// todo
+	/**
+	 * Add a Module to the ModuleList.
+	 * @param module The Module to be added.
+	 * @throws IllegalArgumentException if a Module with the same code already exists in the list
+	 */
+	public void addModule(Module module) throws IllegalArgumentException {
+		if (modules[module.getCode()] != null) {
+			throw new IllegalArgumentException("Module already exists in ModuleList during ModuleList::addModule()");
+		}
+		
+		modules[module.getCode()] = module;
 	}
 	
-	public void editModule(int code, Module module) {
-		// todo
+	/**
+	 * Deletes the Module with number "code" in ModuleList.
+	 * @param code The Module to delete.
+	 * @throws IllegalArgumentException if there is no Module with code number "code"
+	 * @throws IndexOutOfBoundsException if code is not between 1-190
+	 */
+	public void deleteModule(int code) throws IllegalArgumentException, IndexOutOfBoundsException {
+		if (code < 1 || code > 190) {
+			throw new IndexOutOfBoundsException("Invalid code parameter for ModuleList::deleteModule()");
+		} else if (modules[code] == null) {
+			throw new IllegalArgumentException("Module does not exists in ModuleList during ModuleList::deleteModule()");
+		}
+		
+		modules[code] = null;
 	}
-	
 	
 	private Module[] modules;
-	public static int SIZE = 190;
+	private static int SIZE = 190;
 }
