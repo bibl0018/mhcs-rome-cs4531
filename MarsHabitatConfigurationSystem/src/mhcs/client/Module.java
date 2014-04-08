b3762b920d7f648419bb6796517e137cbfa9243f
stowe063@d.umn.edu <stowe063@d.umn.edu@12b34c65-585a-62e0-4ec5-0b54b585a8a1>
2014-04-08 20:19:20 +0000

    Completed Module class.

--- MarsHabitatConfigurationSystem/src/mhcs/client/Module.java
+++ MarsHabitatConfigurationSystem/src/mhcs/client/Module.java
@@ -15,19 +15,43 @@ public class Module {
 	 * @param status The status of the Module;   0-No Repairs Needed, 1-Repairs Needed, 2-Not Repairable
 	 * @throws IndexOutOfBoundsException if any parameters is not within the ranges stated above
 	 */
-	public Module(int code, int xcoord, int ycoord, int turns, int status) throws IndexOutOfBoundsException {
+	public Module(int code, int xcoord, int ycoord, int turns, Status status) throws IndexOutOfBoundsException {
 		
 		// Checks that each parameter is within its range
 		if (code < 1 || code > 190
-			|| turns < 0 || turns > 2
-			|| status < 0 || status > 2)
+			|| turns < 0 || turns > 2)
 			throw new IndexOutOfBoundsException("Parameter out of bounds in Module constructor");
-		
+	 
 		this.code = code;
 		this.xCoord = xcoord;
 		this.yCoord = ycoord;
 		this.turns = turns;
 		this.status = status;
+		
+		// Sets the type based on the code
+		if (code > 0 && code <= 40) {
+			type = Type.PLAIN;
+		} else if (code > 60 && code <= 80) {
+			type = Type.DORMITORY;
+		} else if (code > 90 && code <= 100) {
+			type = Type.SANITATION;
+		} else if (code > 110 && code <= 120) {
+			type = Type.FOOD_WATER;
+		} else if (code > 130 && code <= 134) {
+			type = Type.GYM_RELAXATION;
+		} else if (code > 140 && code <= 144) {
+			type = Type.CANTEEN;
+		} else if (code > 150 && code <= 154) {
+			type = Type.POWER;
+		} else if (code > 160 && code <= 164) {
+			type = Type.CONTROL;
+		} else if (code > 170 && code <= 174) {
+			type = Type.AIRLOCK;
+		} else if (code > 180 && code <= 184) {
+			type = Type.MEDICAL;
+		} else {
+			throw new IndexOutOfBoundsException("Type not yet determined");
+		}
 	}
 	
 	public int getCode() {
@@ -46,16 +70,38 @@ public class Module {
 		return turns;
 	}
 	
-	public int getStatus() {
+	public Status getStatus() {
 		return status;
+	}
+	
+	public Type getType() {
+		return type;
 	}
 	
 	// Instance fields
 	private int code,
 				xCoord,
 				yCoord,
-				turns,
-				status;
+				turns;
+	private Status status;
+	private Type type;
 	
-	// type?
+	public enum Status {
+		UNDAMAGED,
+		DAMAGED,
+		UNUSABLE
+	};
+	
+	public enum Type {
+		PLAIN,
+		DORMITORY,
+		SANITATION,
+		FOOD_WATER,
+		GYM_RELAXATION,
+		CANTEEN,
+		POWER,
+		CONTROL,
+		AIRLOCK,
+		MEDICAL
+	};
 }
