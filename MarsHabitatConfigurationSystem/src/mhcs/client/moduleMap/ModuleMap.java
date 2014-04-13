68ae30fb0f238546e31fef27e1e8e775ef62ed6d
wilh0137@d.umn.edu <wilh0137@d.umn.edu@12b34c65-585a-62e0-4ec5-0b54b585a8a1>
2014-04-13 21:10:46 +0000

    Added comments, moved coordinate gets to inside to the null check. Should do nothing if the module is null.

--- MarsHabitatConfigurationSystem/src/mhcs/client/moduleMap/ModuleMap.java
+++ MarsHabitatConfigurationSystem/src/mhcs/client/moduleMap/ModuleMap.java
@@ -1,5 +1,10 @@
 package mhcs.client.moduleMap;
 
+/**
+ * A visual display of the modules on a grid.
+ * Can be used for any module coordinate combination (pre-configured layouts or configured layouts.
+ * @author Jeremiah Wilhelmy
+ */
 
 import mhcs.client.module.ModuleList;
 import mhcs.client.module.Module;
@@ -9,26 +14,48 @@ import com.google.gwt.user.client.ui.Image;
 
 public class ModuleMap{
 	
+	/**
+	 * Constructs the grid and loads all modules on to the grid.
+	 * @param moduleList The list of pre-sorted modules.
+	 */
 	public void onModuleLoad(ModuleList moduleList){
 		Grid g = new Grid(50,50);
 		
+		//For every module in the module list
 		for(int i = 1; i < 191; i++ ){
 			Module module = moduleList.getModule(i);
-			
-			int xCoord = module.getXCoord();
-			int yCoord = module.getYCoord();
 
+			//If module is actually a module and not just an empty space.
 			if(module != null){
+				int xCoord = module.getXCoord();
+				int yCoord = module.getYCoord();
+				
+				//Set the height and width of each cell in the grid,
+				//can be changed if we need to fit it on a smaller screen.
 				g.getCellFormatter().setWidth(xCoord, yCoord, "25px");
 				g.getCellFormatter().setHeight(xCoord, yCoord, "25px");
+				
+				//Passes the module and coordinates into the setImage function, 
+				//which will change the image at the correct grid coordinate then 
+				//return a new grid with the image changed.
 				g = setImage(g, xCoord, yCoord, module);
 			}
 		}
 				
 	}
 
-	
+	/**
+	 * Takes in a module and coordinates, finds the type of that module, and changes the image of 
+	 * the correct cell on the grid to the image of the type of the passed in module
+	 * @param g The grid without the image changed.
+	 * @param row The Y-coordinate of the module, will also be the same coordinate on the grid.
+	 * @param col The X-coordinate of the module, will also be the same coordinate on the grid.
+	 * @param module The module to be added to the grid.
+	 * @return g A copy of the previous grid with the new module added.
+	 */
 	public Grid setImage(Grid g, int row, int col, Module module){
+		
+		//Detects module type, changes image at coordinate accordingly.
 		if (module.getType().equals("PLAIN")){
 			g.setWidget(row, col, new Image("images/Plain.jpg"));
 		}
