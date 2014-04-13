c3a1765d3680eaaf6836a862c2b8e07078f50d1f
dawso172@d.umn.edu <dawso172@d.umn.edu@12b34c65-585a-62e0-4ec5-0b54b585a8a1>
2014-04-13 20:20:18 +0000

    Got GUI working

--- MarsHabitatConfigurationSystem/src/mhcs/client/MarsHabitatConfigurationSystem.java
+++ MarsHabitatConfigurationSystem/src/mhcs/client/MarsHabitatConfigurationSystem.java
@@ -1,8 +1,14 @@
 package mhcs.client;
 
-import mhcs.client.gui.GUI;
-
+import mhcs.client.gui.AddModulePopup;
 import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.dom.client.Style.Unit;
+import com.google.gwt.user.client.Command;
+import com.google.gwt.user.client.Window;
+import com.google.gwt.user.client.ui.HTML;
+import com.google.gwt.user.client.ui.MenuBar;
+import com.google.gwt.user.client.ui.RootPanel;
+import com.google.gwt.user.client.ui.TabLayoutPanel;
 
 /**
  * Entry point classes define <code>onModuleLoad()</code>.
@@ -12,7 +18,41 @@ public class MarsHabitatConfigurationSystem implements EntryPoint {
 	@Override
 	public void onModuleLoad() {
 		// TODO Auto-generated method stub
-		new GUI(); // This did not work
+		RootPanel rootPanel = RootPanel.get();
+		rootPanel.setSize("995px", "650px");
+		
+		Command cmd = new Command() {
+			public void execute() {
+				Window.alert("You selected a menu item!");
+			}
+		};
+		Command addModulePopupCmd = new Command() {
+			public void execute() {
+				new AddModulePopup(null).show();
+			}
+		};
+		
+		MenuBar theMenu = new MenuBar(true);
+		theMenu.setAnimationEnabled(true);
+		theMenu.addItem("Add module", addModulePopupCmd);
+		theMenu.addItem("Minumum resource path", cmd);
+		theMenu.addItem("Calculate habitats", cmd);
+		theMenu.addSeparator();
+		theMenu.addItem("Log off", cmd);
+		
+		MenuBar menu = new MenuBar();
+		menu.setAnimationEnabled(true);
+		menu.addItem("Menu", theMenu);
+		
+		TabLayoutPanel configTabs = new TabLayoutPanel(2, Unit.EM);
+	    configTabs.add(new HTML(""), "1");
+	    configTabs.add(new HTML(""), "2");
+	    configTabs.add(new HTML(""), "3");
+	    configTabs.add(new HTML(""), "4");
+	    configTabs.setHeight("650px");
+	    
+		rootPanel.add(menu);
+		rootPanel.add(configTabs);
 	}
 	
 }
