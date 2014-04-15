92cf5b4672b5ba9a1a6cef9f0b7090a5bce9d328
wilh0137@d.umn.edu <wilh0137@d.umn.edu@12b34c65-585a-62e0-4ec5-0b54b585a8a1>
2014-04-15 21:29:05 +0000

    Added package and class for minimum configurations. Nothing done yet.

---
+++ MarsHabitatConfigurationSystem/src/minimumConfigurations/MinimumConfigurations.java
@@ -0,0 +1,14 @@
+package minimumConfigurations;
+
+import com.google.gwt.user.client.ui.IsWidget;
+import com.google.gwt.user.client.ui.Widget;
+
+public class MinimumConfigurations implements IsWidget{
+
+	@Override
+	public Widget asWidget() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
