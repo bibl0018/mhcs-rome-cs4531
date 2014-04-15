18f01d2c8a8d04a28707ee5bb96b57c3cf1c5252
dawso172@d.umn.edu <dawso172@d.umn.edu@12b34c65-585a-62e0-4ec5-0b54b585a8a1>
2014-04-15 13:43:46 +0000

    Added comments to make the GUI more readable.

--- MarsHabitatConfigurationSystem/src/mhcs/client/MarsHabitatConfigurationSystem.java
+++ MarsHabitatConfigurationSystem/src/mhcs/client/MarsHabitatConfigurationSystem.java
@@ -8,9 +8,7 @@ import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.dom.client.Style.Unit;
 import com.google.gwt.user.client.Command;
 import com.google.gwt.user.client.Window;
-import com.google.gwt.user.client.ui.HTML;
 import com.google.gwt.user.client.ui.Image;
-import com.google.gwt.user.client.ui.IsWidget;
 import com.google.gwt.user.client.ui.MenuBar;
 import com.google.gwt.user.client.ui.PopupPanel;
 import com.google.gwt.user.client.ui.RootPanel;
@@ -23,16 +21,22 @@ public class MarsHabitatConfigurationSystem implements EntryPoint {
 
 	@Override
 	public void onModuleLoad() {
+
+		// Creates the module list.
 		final ModuleList modList = new ModuleList();
-		
+
+		// Creates the root panel and sizes it.
 		RootPanel rootPanel = RootPanel.get();
 		rootPanel.setSize("995px", "650px");
 
+		// Default command for menu items.
 		Command cmd = new Command() {
 			public void execute() {
 				Window.alert("You selected a menu item!");
 			}
 		};
+
+		// Command to show the add module popup.
 		Command addModulePopupCmd = new Command() {
 			public void execute() {
 				final AddModulePopup popup = new AddModulePopup(modList);
@@ -45,9 +49,11 @@ public class MarsHabitatConfigurationSystem implements EntryPoint {
 				});
 			}
 		};
-		
+
+		// Creates the module map.
 		ModuleMap modMap = new ModuleMap(modList);
 
+		// Creates the menu for the menu bar.
 		MenuBar theMenu = new MenuBar(true);
 		theMenu.setAnimationEnabled(true);
 		theMenu.addItem("Add module", addModulePopupCmd);
@@ -56,10 +62,12 @@ public class MarsHabitatConfigurationSystem implements EntryPoint {
 		theMenu.addSeparator();
 		theMenu.addItem("Log off", cmd);
 
+		// Creates the menu bar and adds the menu to itself.
 		MenuBar menu = new MenuBar();
 		menu.setAnimationEnabled(true);
 		menu.addItem("Menu", theMenu);
 
+		// Creates the tabs for the various configurations and module map.
 		TabLayoutPanel configTabs = new TabLayoutPanel(2, Unit.EM);
 		configTabs.add(new Image("images/MarsModuleLandingArea.jpg"), "Module Map");
 		configTabs.add(new Image("images/MarsModuleLandingArea.jpg"), "1");
@@ -69,6 +77,7 @@ public class MarsHabitatConfigurationSystem implements EntryPoint {
 		configTabs.insert(modMap, 0);
 		configTabs.setHeight("650px");
 
+		// Adds everything to the root panel.
 		rootPanel.add(menu);
 		rootPanel.add(configTabs);
 	}
