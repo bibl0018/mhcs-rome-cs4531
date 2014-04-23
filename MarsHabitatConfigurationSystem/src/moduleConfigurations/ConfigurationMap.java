4f5ee3255475bd29d1970b9a88fe723784007d8c
stowe063@d.umn.edu <stowe063@d.umn.edu@12b34c65-585a-62e0-4ec5-0b54b585a8a1>
2014-04-23 21:36:11 +0000

    Fixed remaining checkstyle errors in my code.

--- MarsHabitatConfigurationSystem/src/moduleConfigurations/ConfigurationMap.java
+++ MarsHabitatConfigurationSystem/src/moduleConfigurations/ConfigurationMap.java
@@ -10,7 +10,19 @@ import com.google.gwt.user.client.ui.Widget;
 
 public class ConfigurationMap extends Panel {
 
-	
+	public static final int CENTER = 12;
+	public static final int SIZE = 25;
+	public static final int WEST_LIMIT = 10;
+	public static final int EAST_LIMIT = 14;
+	public static final int ONE = 1;
+	public static final int TWO = 2;
+	public static final int THREE = 3;
+	public static final int FOUR = 4;
+	public static final int FIVE = 5;
+	public static final int SIX = 6;
+	public static final int SEVEN = 7;
+	public static final int EIGHT = 8;
+	public static final int NINE = 9;
 	
 	@Override
 	public Iterator<Widget> iterator() {
@@ -19,7 +31,7 @@ public class ConfigurationMap extends Panel {
 	}
 
 	@Override
-	public boolean remove(Widget child) {
+	public boolean remove(final Widget child) {
 		// TODO Auto-generated method stub
 		return false;
 	}
@@ -29,7 +41,7 @@ public class ConfigurationMap extends Panel {
 	//Function for constructing a configuration. This will probably not be in this class.
 	public void calculateConfiguration(final ModuleList modules) {
 		
-		Type[][] config = new Type[25][25];
+		Type[][] config = new Type[SIZE][SIZE];
 		int plain = modules.getNumOfPlain();
 		int airlock = modules.getNumOfAirlock();
 		int power = modules.getNumOfPower();
@@ -40,42 +52,118 @@ public class ConfigurationMap extends Panel {
 		int gym = modules.getNumOfGym();
 		int medical = modules.getNumOfMedical();
 		int control = modules.getNumOfControl();
-		
+		int northTip = 0;
+		int southTip = 0;
+		int eastTip = 0;
+		int westTip = 0;
 		
 		// Places the first plain module in the center of the map.
-		config[12][12] = Type.PLAIN;
+		config[CENTER][CENTER] = Type.PLAIN;
 		plain -= 1;
 		
 		// Places the remaining plain modules in four wings in a cross formation.
-		for (int wings = 4; wings > 0; wings -= 1) {
+		for (int wings = FOUR; wings > 0; wings -= 1) {
 			int i = 0;
-			switch (wings) {
-			case 4: 
+			if (wings == FOUR) {
 				for (i = 0; i < (plain/wings); i += 1) {
-					config[12][12 + i + 1] = Type.PLAIN;
+					config[CENTER + i + 1][CENTER] = Type.PLAIN;
 				}
-			case 3:
+				northTip = CENTER + i;
+			} else if (wings == THREE) {
 				for (i = 0; i < (plain/wings); i += 1) {
-					config[12 + i + 1][12] = Type.PLAIN;
+					config[CENTER][CENTER + i + 1] = Type.PLAIN;
 				}
-			case 2:
+				eastTip = CENTER + i;
+			} else if (wings == TWO) {
 				for (i = 0; i < (plain/wings); i += 1) {
-					config[12][12 - i - 1] = Type.PLAIN;
-				}
-			case 1:
+					config[CENTER - i - 1][CENTER] = Type.PLAIN;
+					}
+				southTip = CENTER - i;
+			} else if (wings == ONE) {
 				for (i = 0; i < (plain/wings); i += 1) {
-					config[12 - i - 1][12] = Type.PLAIN;
+					config[CENTER][CENTER - i - 1] = Type.PLAIN;
 				}
+				westTip = CENTER - i;
 			}
 			plain -= i;
 		}
 		
-		// Builds two dormitory wings
-		// Dormitory to Sanitation 2:1
-		// Sanitation not next to Canteen or Food/Water
-		// Sanitation next to Gym
-		// Airlock not next to dormitory
+		// Place airlocks at tips of wings.
+		for (int i = 0; i < airlock; i += 1) {
+			if (i == 0) {
+				config[northTip + 1][CENTER] = Type.AIRLOCK;
+			} else if (i == 1){
+				config[CENTER][eastTip + 1] = Type.AIRLOCK;
+			} else if (i == 2){
+				config[southTip - 1][CENTER] = Type.AIRLOCK;
+			} else if (i == THREE) {
+				config[CENTER][westTip - 1] = Type.AIRLOCK;
+			}
+		}
 		
+		// Place medical modules by the airlocks.
+		for (int i = 0; i < medical; i += 1) {
+			if (i == 0) {
+				config[northTip][CENTER - 1] = Type.MEDICAL;
+			} else if (i == 1){
+				config[CENTER + 1][eastTip] = Type.MEDICAL;
+			} else if (i == 2){
+				config[southTip][CENTER + 1] = Type.MEDICAL;
+			} else if (i == THREE) {
+				config[CENTER - 1][westTip] = Type.MEDICAL;
+			}
+		}
+		
+		// Place control modules across from by the airlocks and across from medical.
+		for (int i = 0; i < control; i += 1) {
+			if (i == 0) {
+				config[northTip][CENTER + 1] = Type.CONTROL;
+			} else if (i == 1){
+				config[CENTER - 1][eastTip] = Type.CONTROL;
+			} else if (i == 2){
+				config[southTip][CENTER - 1] = Type.CONTROL;
+			} else if (i == THREE) {
+				config[CENTER + 1][westTip] = Type.CONTROL;
+			}
+		}
+		
+		// Place power modules in east and west wings by control and medical
+		for (int i = 0; i < power; i += 1) {
+			if (i == 0 && (westTip + 1) < WEST_LIMIT) {
+				config[CENTER + 1][westTip + 1] = Type.POWER;
+			} else if (i == 1 && (eastTip - 1) > EAST_LIMIT){
+				config[CENTER + 1][eastTip - 1] = Type.POWER;
+			} else if (i == 2 && (westTip + 2) < WEST_LIMIT){
+				config[CENTER - 1][westTip + 1] = Type.POWER;
+			} else if (i == THREE && (eastTip - 1) > EAST_LIMIT) {
+				config[CENTER - 1][eastTip - 1] = Type.POWER;
+			}
+		}
+		
+		// Place water modules in east and west wings by power modules
+		for (int i = 0; i < water; i += 1) {
+			if (i == 0 && (westTip + 2) < WEST_LIMIT) {
+				config[CENTER + 1][westTip + 2] = Type.FOOD_WATER;
+			} else if (i == 1 && (eastTip - 2) > EAST_LIMIT){
+				config[CENTER + 1][eastTip - 2] = Type.FOOD_WATER;
+			} else if (i == 2 && (westTip + 2) < WEST_LIMIT){
+				config[CENTER - 1][westTip + 2] = Type.FOOD_WATER;
+			} else if (i == THREE && (eastTip - 2) > EAST_LIMIT) {
+				config[CENTER - 1][eastTip - 2] = Type.FOOD_WATER;
+			} else if (i == FOUR && (westTip + THREE) < WEST_LIMIT) {
+				config[CENTER + 1][westTip + THREE] = Type.FOOD_WATER;
+			} else if (i == FIVE && (eastTip - THREE) > EAST_LIMIT){
+				config[CENTER + 1][eastTip - THREE] = Type.FOOD_WATER;
+			} else if (i == SIX && (westTip + THREE) < WEST_LIMIT){
+				config[CENTER - 1][westTip + THREE] = Type.FOOD_WATER;
+			} else if (i == SEVEN && (eastTip - THREE) > EAST_LIMIT) {
+				config[CENTER - 1][eastTip - THREE] = Type.FOOD_WATER;
+			} else if (i == EIGHT && (westTip + FOUR) < WEST_LIMIT) {
+				config[CENTER + 1][westTip + FOUR] = Type.FOOD_WATER;
+			} else if (i == NINE && (eastTip - FOUR) > EAST_LIMIT){
+				config[CENTER + 1][eastTip - FOUR] = Type.FOOD_WATER;
+			}
+		}
 	}
 	
 }
