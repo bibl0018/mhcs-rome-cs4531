472ceebacbefd4a9e65a8ff235c45f6b5dc83a0c
dawso172@d.umn.edu <dawso172@d.umn.edu@12b34c65-585a-62e0-4ec5-0b54b585a8a1>
2014-05-01 00:38:48 +0000

    Part of the update for showing the weather under the menubar

--- MarsHabitatConfigurationSystem/src/mhcs/client/weather/Weather.java
+++ MarsHabitatConfigurationSystem/src/mhcs/client/weather/Weather.java
@@ -11,31 +11,31 @@ import com.google.gwt.json.client.JSONParser;
 import com.google.gwt.json.client.JSONValue;
 import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.ui.HasVerticalAlignment;
+import com.google.gwt.user.client.ui.HasVerticalAlignment.VerticalAlignmentConstant;
 import com.google.gwt.user.client.ui.Image;
 import com.google.gwt.user.client.ui.IsWidget;
 import com.google.gwt.user.client.ui.Label;
-import com.google.gwt.user.client.ui.RootLayoutPanel;
-import com.google.gwt.user.client.ui.VerticalPanel;
+import com.google.gwt.user.client.ui.HorizontalPanel;
 import com.google.gwt.user.client.ui.Widget;
 
 public class Weather implements IsWidget {
 
-	private static final String PANEL_WIDTH = "3120px";
-	private static final String PANEL_HEIGHT = "1600px";
 	private static final int STATUS_CODE_OK = 200;
+	private static final HorizontalPanel HP = new HorizontalPanel();
 
 	public Weather() {
 	}
 
 	@Override
 	public Widget asWidget() {
-		RootLayoutPanel.get().setSize(PANEL_WIDTH, PANEL_HEIGHT);
 		
-		final Image logo = new Image("images/wundergroundLogo_4c.jpg");
-		logo.setWidth("107px");
-		logo.setHeight("63px");
+		final Image logo = new Image("images/wundergroundLogo_4c_horz.jpg");
+		logo.setWidth("90px");
+		logo.setHeight("21px");
 		logo.addStyleName("wundergroundLogo");
 
+		// Uncomment below for testing on my website.
+		//String proxy = "http://d.umn.edu/~dawso172/war/Weather.php?url=";
 		String proxy = "http://www.d.umn.edu/~stowe063/war/Weather.php?url="; 
 		String url = proxy+"http://api.wunderground.com/api/0a4dcfa0929f5d9b/conditions/q/55812.json"; 
 
@@ -61,28 +61,20 @@ public class Weather implements IsWidget {
 				}
 
 				private void update(final String rt) {
-					VerticalPanel vp = new VerticalPanel(); 
-					//vp.add(new Label(rt)); //TO VIEW 
-
 					String sAll = rt; 
 					JSONObject jA = (JSONObject)JSONParser.parseLenient(sAll); 
 					JSONValue jTry = jA.get("current_observation"); 
 
 					JSONObject jB = (JSONObject)JSONParser.parseLenient(jTry.toString()); 
-					JSONValue temp = jB.get("temp_c"); 
-					JSONValue icon = jB.get("icon_url");
+					JSONValue temp = jB.get("temp_c");
 					JSONValue visibility = jB.get("visibility_km"); 
 
 					String sTemp = temp.toString(); 
 					String sVisibility = visibility.toString(); 
 
-					vp.add(logo);
-					vp.add(new Label("Temp in C: " + sTemp + " ")); 
-					vp.add(new Label("Visibility in Km: " + sVisibility));
-					vp.setSpacing(0);
-					vp.setVerticalAlignment(HasVerticalAlignment.ALIGN_TOP);
-					vp.addStyleName("weatherPanel");
-					RootLayoutPanel.get().add(vp);
+					HP.add(logo);
+					HP.add(new Label("Temp in C: " + sTemp + ", Visibility in Km: " + sVisibility));
+					HP.addStyleName("weatherPanel");
 
 				} 
 			}); 
@@ -90,9 +82,8 @@ public class Weather implements IsWidget {
 		catch (RequestException e) { 
 			Window.alert("RequestException: Couldn't retrieve JSON"); 
 		}
-
-		RootLayoutPanel.get().addStyleName("rootWeatherPanel");
-		return RootLayoutPanel.get();
+		
+		return HP;
 	}
 
 }
